c3582437f755905987c0f416f272ee0c9555297c
dgrimaldi <grimi.david@gmail.com>
2019-09-22 08:48:33 +0200

    modal incomplete, added of routeLink

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {TokenService} from "../tokens-service/token.service";
 import {Token} from "../tokens/token";
 import {MatDialog} from "@angular/material";
-import {MatDialogModule} from '@angular/material/dialog';
 import {IssueModalComponent} from "../issue-modal/issue-modal.component";
 import {ActivatedRoute, Router} from "@angular/router";
 
@@ -12,28 +11,36 @@ import {ActivatedRoute, Router} from "@angular/router";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  // Array of tokens
   tokens: Token[];
 
   /**
    *
-   * @param tokenService asking for TokenService
-   * to be injected in this component
+   * @param tokenService
+   * @param modal
+   * @param router
+   * @param route
    */
   constructor(tokenService: TokenService,
-              public modal: MatDialog) {
+              public modal: MatDialog,
+              private router: Router,
+              private route: ActivatedRoute) {
     this.tokens = tokenService.getTokens();
   }
 
   ngOnInit() {
   }
 
+  /**
+   *
+   */
   openModal(): void {
     const modalRef = this.modal.open(IssueModalComponent, {
       width: '250px'
-    })
+    });
     modalRef.afterClosed().subscribe(res => {
-      //this.router.navigate(['../'], {relativeTo: this.route});
-    })
+      this.router.navigate(['../'], {relativeTo: this.route});
+    });
   };
 
 }
